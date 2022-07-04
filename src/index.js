98ee92647dbd46db87cb11522b7fd9da0eda9df2
Shane Howe <shanehowe100@hotmail.com>
2022-07-03 21:31:43 -0700

    build setting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from '/Users/NewUser/Desktop/developer-projects/react/jamming/src/components/App/App.js';
+import App from './components/App'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
